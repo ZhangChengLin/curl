dba865f4a50e45aae6f827ed8e2ee82f3e6ef267
zhangchenglin <469946668@qq.com>
2020-12-17 20:29:00 +0800

    Available in Composer package

--- example/requestData.php
+++ example/requestData.php
@@ -1,5 +1,8 @@
 <?php
-require_once 'vendor/autoload.php';
+
+// Available in Composer package
+$ComposerVendorDir = dirname(dirname(dirname(__DIR__)));
+require_once $ComposerVendorDir . '/autoload.php';
 
 $RequestURL = 'https://example.org';
 $SendData = [
