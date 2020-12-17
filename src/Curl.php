001e3aa9c75539c1b8679b1710dbee91e5b6d2e6
zhangchenglin <469946668@qq.com>
2020-12-17 19:16:15 +0800

    Add the Opt value of the instance of the abstract class

--- src/Curl.php
+++ src/Curl.php
@@ -26,22 +26,20 @@ abstract class Curl implements CurlInterface
     public function __construct(string $RequestURL = '', array $SendData = array(), string $RequestMethod = null)
     {
         self::setCurl();
-
-        // @link https://www.php.net/manual/en/function.curl-setopt.php
-        $this->Curl->setOpt(CURLOPT_TIMEOUT_MS, 4000);//设置cURL允许执行的最长毫秒数;
-        $this->Curl->setOpt(CURLOPT_DNS_CACHE_TIMEOUT, 1200);//设置在内存中保存DNS信息的时间，默认为120秒。
-
-        $this->Curl->setOpt(CURLOPT_AUTOREFERER, true);//TRUE 时将根据 Location: 重定向时，自动设置 header 中的Referer:信息。
-
-        $this->Curl->setOpt(CURLOPT_FOLLOWLOCATION, true);//TRUE 时将会根据服务器返回 HTTP 头中的 "Location: " 重定向。（注意：这是递归的，"Location: " 发送几次就重定向几次，除非设置了 CURLOPT_MAXREDIRS，限制最大重定向次数。）。
-        $this->Curl->setOpt(CURLOPT_MAXREDIRS, 2);//指定最多的 HTTP 重定向次数，这个选项是和CURLOPT_FOLLOWLOCATION一起使用的。
-
 //        $this->Curl->setCookie('key', 'value');
 
         if (isset($RequestMethod) && ($RequestMethod === 'get' || $RequestMethod === 'post')) {
             // @link https://www.php.net/manual/en/function.curl-setopt.php
             $this->Curl->setUserAgent($_SERVER['HTTP_USER_AGENT']);
             $this->Curl->setReferrer($_SERVER['HTTP_REFERER'] ?? '-');
+
+            $this->Curl->setOpt(CURLOPT_TIMEOUT_MS, 4000);//设置cURL允许执行的最长毫秒数;
+            $this->Curl->setOpt(CURLOPT_DNS_CACHE_TIMEOUT, 1200);//设置在内存中保存DNS信息的时间，默认为120秒。
+
+            $this->Curl->setOpt(CURLOPT_AUTOREFERER, true);//TRUE 时将根据 Location: 重定向时，自动设置 header 中的Referer:信息。
+
+            $this->Curl->setOpt(CURLOPT_FOLLOWLOCATION, true);//TRUE 时将会根据服务器返回 HTTP 头中的 "Location: " 重定向。（注意：这是递归的，"Location: " 发送几次就重定向几次，除非设置了 CURLOPT_MAXREDIRS，限制最大重定向次数。）。
+            $this->Curl->setOpt(CURLOPT_MAXREDIRS, 2);//指定最多的 HTTP 重定向次数，这个选项是和CURLOPT_FOLLOWLOCATION一起使用的。
 
             if ($RequestMethod === 'get') {
                 self::get($RequestURL, $SendData);
